344865f8b7d436df2836ef28e3c16144e9b71da5
Abhishek victor <abhishekvictor03@gmail.com>
2026-03-07 14:13:46 +0530

    componets

--- App.jsx
+++ App.jsx
@@ -1,23 +1,41 @@
 import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Alert, TouchableHighlight, Pressable } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
-const name = 'khushi';
-let age = 70
-function fruit(){
-  return 'apple'
-}
-
 const App = () => {
   return (
     <SafeAreaView>
 
-      <Text style={{ fontSize: 30 }}>Components {name}</Text>
-      <Text style={{ fontSize: 30 }}>HI My age is {age}</Text>
-      <Text style={{ fontSize: 30 }}>{fruit()}</Text>
-      <Text stytl={{fontSize:25}}> {age >= 30 ? 'Perfact age': "under age"}</Text>
-      <Button title='press me '></Button>
+      <Text style={{ fontSize: 30 }}>Components </Text>
+      <UserDate/>
+      <UserDate/>
+      <Colloge/>
+
+
     </SafeAreaView>
+  );
+};
+
+const UserDate = ()=>{
+  return(
+    <View>
+      <Text style={{ fontSize: 20 }}>Name: Anil </Text>
+      <Text style={{ fontSize: 20 }}>age : 29 </Text>
+      <Text style={{ fontSize: 20 }}>email: Anil@gmail.com </Text>
+      
+    </View>
+
+
+  )
+}
+const Colloge = ()=>{
+  return(
+    <View>
+      <Text style={{ fontSize: 20 }}>Name: TATA </Text>
+      <Text style={{ fontSize: 20 }}>emp : 29 </Text>
+      <Text style={{ fontSize: 20 }}>Producte: abs </Text>
+    </View>
+
+
   )
 }
 
