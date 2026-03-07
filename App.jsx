ff9a543454655b25c2c3291e19d3115aefed565a
Abhishek victor <abhishekvictor03@gmail.com>
2026-03-07 16:06:39 +0530

    learn all the component and create and calling he compoent

--- App.jsx
+++ App.jsx
@@ -1,16 +1,15 @@
 import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Alert, TouchableHighlight, Pressable } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Colloge from './Componets/componetData.js'
 const App = () => {
   return (
     <SafeAreaView>
 
+     <Colloge/>
       <Text style={{ fontSize: 30 }}>Components </Text>
       <UserDate/>
       <UserDate/>
-      <Colloge/>
-
-
     </SafeAreaView>
   );
 };
@@ -22,17 +21,6 @@ const UserDate = ()=>{
       <Text style={{ fontSize: 20 }}>age : 29 </Text>
       <Text style={{ fontSize: 20 }}>email: Anil@gmail.com </Text>
       
-    </View>
-
-
-  )
-}
-const Colloge = ()=>{
-  return(
-    <View>
-      <Text style={{ fontSize: 20 }}>Name: TATA </Text>
-      <Text style={{ fontSize: 20 }}>emp : 29 </Text>
-      <Text style={{ fontSize: 20 }}>Producte: abs </Text>
     </View>
 
 
---
+++ Componets/componetData.js
@@ -0,0 +1,17 @@
+import {Text, View} from 'react-native'
+
+// user data
+// company Data
+const Colloge = ()=>{
+  return(
+    <View>
+      <Text style={{ fontSize: 20 }}>Name: TATA </Text>
+      <Text style={{ fontSize: 20 }}>emp : 30 </Text>
+      <Text style={{ fontSize: 20 }}>Producte: car </Text>
+    </View>
+
+
+  )
+}
+
+export default Colloge;
