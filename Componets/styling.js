c7d61dac8d9f09991c248ad60c3077b26563a718
Abhishek victor <abhishekvictor03@gmail.com>
2026-03-10 08:50:49 +0530

    learn the styling

--- Componets/styling.js
+++ Componets/styling.js
@@ -2,13 +2,32 @@ import { Text, View, StyleSheet } from 'react-native'
 const StyleApp = () => {
     return (
 
-        <View style ={{fontSize:30}}>
-            <Text>Hadding for Text</Text>
+        <View>
+            <Text  style ={{fontSize:30}}>Hadding for Text</Text>
+            <Text style ={Style.textbox}>style 1 for Text</Text>
+            <Text>sttyl 2 for Text</Text>
         </View>
 
 
     )
 }
 
+const Style = StyleSheet.create({
+    textbox:{
+        fontSize:30,
+        color: "#dfbebe",
+        backgroundColor: 'grey',
+        padding:10,
+        margin:15,
+        borderRadius:10,
+        borderWidth:2,
+        
+
+
+
+
+
+    }
+})
 
 export default StyleApp
