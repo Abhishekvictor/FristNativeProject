2905cc842a49ab339f3f60b7631695d8e19cae01
Abhishek victor <abhishekvictor03@gmail.com>
2026-03-18 13:49:04 +0530

    create the form page and clear button and display  the date

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Alert, TouchableHighlight, Pressable,TextInput } from 'react-native'
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Alert, TouchableHighlight, Pressable, TextInput } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Colloge from './Componets/componetData.js'
@@ -8,23 +8,79 @@ import StyleApp from './Componets/styling.js'
 
 
 const App = () => {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const[display,setDisplay] = useState('');
+
+  const restFormDate = ()=>{
+    setDisplay(false)
+    setName('')
+    setEmail('')
+    setPassword('')
+  }
 
   return (
     <SafeAreaView>
-      <Text style={{fontSize:20,color:"grey"}}> Enter Your Name: {name} </Text>
-    <TextInput placeholder='Enter the name' style={{borderColor:'blue', color:'Green', borderWidth:2, borderRadius:10,}}
-    onChangeText={(Text) =>setName(Text)}
-    value={name}
-    />
-    <Button
-    title='clear Input Value' onPress={()=>setName('')}
-    />
+      {/* Hedding of form page */}
+      <Text style={{ fontSize: 30 }}> Simple Form in React Native</Text>
       
-    
+      {/* User Name */}
+      <TextInput style={style.textInput}
+        placeholder='Enter the User Name'
+        onChangeText={(text) => setName(text)}
+        value={name}
+      />
+
+      {/* User Email */}
+      <TextInput style={style.textInput}
+        placeholder='Enter Your Email'
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+
+      {/* Enter your Password */}
+      <TextInput style={style.textInput}
+        placeholder='Enter Your Password '
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry={true}
+      />
+
+      {/* Button of form page  */}
+      <View style={{marginBottom:10}}>
+      <Button color={'lightgreen'} title='Print Details'
+      onPress={()=>setDisplay(true)}
+      />
+      </View>
+      <Button title='Clear Details' onPress={restFormDate}/>
+
+      {/* display the values */}
+     <View>
+      {
+        display ? 
+        <View>
+          <Text style={{fontSize:20}}>This is the User Name: {name}</Text>
+          <Text style={{fontSize:20}}>This is the User Email: {email}</Text>
+          <Text style={{fontSize:20}}>This is the User Password: {password}</Text>
+        </View>
+        : null
+      }
+     </View>
+
     </SafeAreaView>
   )
 }
+
+const style = StyleSheet.create({
+  textInput: {
+    fontSize: 18,
+    color: 'blue',
+    borderWidth: 2,
+    borderColor: 'blue',
+    margin: 10,
+  }
+})
 
 
 export default App
