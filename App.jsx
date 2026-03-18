1f8660170095335465ff90f0546f95d009ad0a98
Abhishek victor <abhishekvictor03@gmail.com>
2026-03-18 21:58:32 +0530

    Create the form page with componet style and exported to main app

--- App.jsx
+++ App.jsx
@@ -5,83 +5,18 @@ import Colloge from './Componets/componetData.js'
 import Btn from './Componets/button.js'
 import PropsUser from './Componets/Props.js'
 import StyleApp from './Componets/styling.js'
+import Formpage from './Componets/login.js'
 
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const[display,setDisplay] = useState('');
-
-  const restFormDate = ()=>{
-    setDisplay(false)
-    setName('')
-    setEmail('')
-    setPassword('')
-  }
-
   return (
     <SafeAreaView>
-      {/* Hedding of form page */}
-      <Text style={{ fontSize: 30 }}> Simple Form in React Native</Text>
       
-      {/* User Name */}
-      <TextInput style={style.textInput}
-        placeholder='Enter the User Name'
-        onChangeText={(text) => setName(text)}
-        value={name}
-      />
-
-      {/* User Email */}
-      <TextInput style={style.textInput}
-        placeholder='Enter Your Email'
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-      />
-
-      {/* Enter your Password */}
-      <TextInput style={style.textInput}
-        placeholder='Enter Your Password '
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-        secureTextEntry={true}
-      />
-
-      {/* Button of form page  */}
-      <View style={{marginBottom:10}}>
-      <Button color={'lightgreen'} title='Print Details'
-      onPress={()=>setDisplay(true)}
-      />
-      </View>
-      <Button title='Clear Details' onPress={restFormDate}/>
-
-      {/* display the values */}
-     <View>
-      {
-        display ? 
-        <View>
-          <Text style={{fontSize:20}}>This is the User Name: {name}</Text>
-          <Text style={{fontSize:20}}>This is the User Email: {email}</Text>
-          <Text style={{fontSize:20}}>This is the User Password: {password}</Text>
-        </View>
-        : null
-      }
-     </View>
-
+      {/* From page componet */}
+      <Formpage />
     </SafeAreaView>
   )
 }
-
-const style = StyleSheet.create({
-  textInput: {
-    fontSize: 18,
-    color: 'blue',
-    borderWidth: 2,
-    borderColor: 'blue',
-    margin: 10,
-  }
-})
-
 
 export default App
 
