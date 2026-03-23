8e7ea95df5c0981f699bd4883cf6b17dedb44595
Abhishek victor <abhishekvictor03@gmail.com>
2026-03-23 10:54:32 +0530

    learn the section list

--- Componets/sectionLIst.js
+++ Componets/sectionLIst.js
@@ -1,8 +1,35 @@
 import {View, Text, SectionList} from 'react-native'
 
+const users = [
+    {
+        id:1,
+        name:'abhishek',
+        data:['PHP','Reat js','Angular']
+    },
+     {
+        id:1,
+        name:'peter',
+        data:['java','C ','C++']
+    },
+     {
+        id:1,
+        name:'Tony',
+        data:['Python','React native','HTML']
+    },
+]
 const SectionListex = () =>{
     return(
-        <Text>this is the coo</Text>
+        <View>
+            <SectionList
+            sections={users}
+            renderItem={({item})=><Text>{item}</Text>}
+            renderSectionHeader={({section:{name}})=>(
+                <Text style={{fontSize:25, color:'red',}}>{name}</Text>
+            )}
+            />
+
+            
+        </View>
     )
 }
 
