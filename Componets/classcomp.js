20e3274a7aba67089c2b505869403a97100ed0bb
Abhishek victor <abhishekvictor03@gmail.com>
2026-04-10 14:11:58 +0530

    class copont

--- Componets/classcomp.js
+++ Componets/classcomp.js
@@ -2,14 +2,24 @@ import React,{Component} from 'react';
 import { Alert, Button,Text, TextInput, View } from 'react-native';
 
 class Student extends Component{
-    fruit =()=>{
-        console.warn("Apple");
+
+    constructor(){
+        super();
+        this.state={
+            
+            name:'Abhihsek'
+        }
     }
+
+    updateName(body){
+        this.setState({name:body})
+    }
+   
     render(){
         return(
             <View>
-                <Text style={{fontSize:30,color:'red'}}> class Component</Text>
-                <TextInput placeholder='enter your name'></TextInput>
+                <Text style={{fontSize:30,color:'red'}}>{this.state.name}</Text>
+                <TextInput placeholder='enter your name' onChangeText={(text)=>this.updateName(text)}></TextInput>
                 <Button title='Press Me' onPress={this.fruit}></Button>
             </View>
         )
