336df78621fb02259b78e83bf073afa99e702fe0
Abhishek victor <abhishekvictor03@gmail.com>
2026-04-13 20:27:57 +0530

    learn the styling part of the navigation

--- Componets/stacknavigation.js
+++ Componets/stacknavigation.js
@@ -1,35 +1,46 @@
 import { View, Text, StatusBar, Button } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 
 const Stack = createNativeStackNavigator();
- const App2 = () =>{
-    return(
+const App2 = () => {
+    return (
         <NavigationContainer>
-            <Stack.Navigator>
-            <Stack.Screen name='Home' component={Home}/>
-            <Stack.Screen name='Login' component={Login}/>
+            <Stack.Navigator screenOptions={{
+                headerStyle: {
+                    backgroundColor: 'lightblue'
+                },
+                headerTitleStyle: { fontSize: 25 },
+                headerTintColor: 'white',
+
+            }
+            } >
+                <Stack.Screen name='Home' component={Home}
+
+                />
+                <Stack.Screen name='Login' component={Login} />
             </Stack.Navigator>
         </NavigationContainer>
-        
 
 
-)}
 
-const Home = (props)=>{
-    return(
-        <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+    )
+}
+
+const Home = (props) => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text> This is Home Screen </Text>
-            <Button title='go to home page' onPress={()=>props.navigation.navigate('Login')}></Button>
+            <Button title='go to home page' onPress={() => props.navigation.navigate('Login')}></Button>
         </View>
     )
 }
 
-const Login = () =>{
-    return(
-        <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+const Login = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text> This is Login Screen </Text>
-            
+
         </View>
     )
 }
