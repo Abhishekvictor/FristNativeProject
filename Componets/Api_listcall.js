c023173525b2e9c3f41c7a4642b18d899b3e4700
Abhishek victor <abhishekvictor03@gmail.com>
2026-05-05 17:19:39 +0530

    compoletethe list calling

--- Componets/Api_listcall.js
+++ Componets/Api_listcall.js
@@ -1,11 +1,11 @@
-import { View, Text, StatusBar } from 'react-native'
+import { View, Text, StatusBar, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 const App2 = () => {
 
-    const [data, setdata] = useState(undefined)
+    const [data, setdata] = useState([])
 
     const getAipDAta = async () => {
-        const url = 'https://jsonplaceholder.typicode.com/todos/1'
+        const url = 'https://jsonplaceholder.typicode.com/posts'
         let result = await fetch(url)
         result = await result.json()
         setdata(result)
@@ -19,15 +19,24 @@ const App2 = () => {
 
 
     return (
-        <View>
+        <ScrollView>
 
             <View><Text> API Call</Text></View>
             {
-                data ?<View>
-                        <Text>{data.id}</Text>
-                    </View> : null
+                data.length ?
+                    data.map((item) => {
+                        return (
+                            <View key={item.id} style={{padding:10, borderBottomColor:'#ccc', borderBottomWidth:1}}>
+                                <View><Text>{item.id}</Text></View>
+                                <View><Text>{item.title}</Text></View>
+                                <View><Text>{item.body}</Text></View>
+                            </View>
+                        )
+                    })
+
+                    : <Text>We did not get the data</Text>
             }
-        </View>
+        </ScrollView>
     )
 }
 
